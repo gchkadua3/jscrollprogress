180adbfc75af6c24e73ea0079bba6330fdd9af60
gchkadua3 <120250936+gchkadua3@users.noreply.github.com>
2023-07-26 13:42:28 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", () => {
   const progressBar = document.querySelector(".progress-bar");
 
   
